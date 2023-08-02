aac5913aa970218822b0bdcf218b7414b3695d4f
adamj <adamos00@op.pl>
2023-08-02 14:05:06 +0200

    Startup app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from "./posts/post-list/post-list.component";
+import { PostItemComponent } from './posts/post-list/post-item/post-item.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostItemComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
