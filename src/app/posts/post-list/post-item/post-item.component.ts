6c6eec854c25bdeed8a6a15bd7c4875f1d5af580
adamj <adamos00@op.pl>
2023-08-02 14:02:50 +0200

    Added post structure

---
+++ src/app/posts/post-list/post-item/post-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-item',
+  templateUrl: './post-item.component.html',
+  styleUrls: ['./post-item.component.css']
+})
+export class PostItemComponent {
+
+
+}
